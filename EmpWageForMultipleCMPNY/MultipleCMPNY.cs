da7b0bae580b4d18b2af2183811dc39dd153c4f4
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-05 22:41:08 +0530

    EmpWageDoneForMultipleCompanys

---
+++ EmpWageForMultipleCMPNY/MultipleCMPNY.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageForMultipleCMPNY
+{
+   public class MultipleCMPNY
+    {
+        int isFullTime = 1;
+        int isPartTime = 2;
+        public int calculateEmpWage(String company, int empRatePerHour, int NumOfWorkingDays, int MaxHoursPerMonth)
+        {
+            int empHours = 0, totalEmpHours = 0, totalWorkingDays = 0;
+            while (totalEmpHours <= MaxHoursPerMonth && totalWorkingDays < NumOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHours = 8;
+                        break;
+
+                    case 2:
+                        empHours = 4;
+                        break;
+
+                    default:
+                        empHours = 0;
+                        break;
+
+                }
+                totalEmpHours += empHours;
+            }
+            int totalEmpwage = totalEmpHours * empRatePerHour;
+            Console.WriteLine("Total emp wage=" + totalEmpwage);
+            Console.WriteLine("EmpWageForCompany" + company);
+            return totalEmpwage;
+        }
+    }
+}
---
+++ EmpWageForMultipleCMPNY/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmpWageForMultipleCMPNY
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            MultipleCMPNY obj1 = new MultipleCMPNY();
+            obj1.calculateEmpWage("DMart",20, 2, 10);
+            obj1.calculateEmpWage("TCS", 20, 10, 20);
+        }
+    }
+}
