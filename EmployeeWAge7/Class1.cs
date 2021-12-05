71d88686716e205f7987aaba337d12a23468d05f
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-05 22:34:20 +0530

    RefactoringTheCodeByUsingClass

---
+++ EmployeeWAge7/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWAge7
+{
+    public class Class1
+    {
+        int empWage = 0;
+        int empHrs = 0;
+        int empRatePerHr = 20;
+        int Max_Working_Hr_Per_Month = 100;
+        int Max_Working_Days_In_Month = 20;
+        int Num_Working_Hr_Per_Month = 0;
+        int Num_Working_Days_In_Month = 0;
+                 public int ComputeEmpWage()
+                  {
+
+                while ((Num_Working_Hr_Per_Month <= Max_Working_Hr_Per_Month && Num_Working_Days_In_Month <= Max_Working_Days_In_Month))
+                  {
+                Num_Working_Days_In_Month++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                Num_Working_Hr_Per_Month += empHrs;
+                empWage = empRatePerHr * Num_Working_Hr_Per_Month;
+            }
+            Console.WriteLine("TotalEmployeeWageOfReachingMaxHrsAndDays: " + empWage);
+            return ComputeEmpWage();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EmployeeWAge7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWAge7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+            obj1.ComputeEmpWage();
+        }
+    }
+}
