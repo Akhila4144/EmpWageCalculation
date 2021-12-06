f41acf5d105e29c4efa2418799da0f724889fc5a
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-06 10:20:06 +0530

    DoneEmpWageForSavingTotalWageOfEachCompany

---
+++ EmployeeWage_9/CalculateWage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage_9
+{
+   public class CalculateWage
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+
+        public string Company;
+        public int empRatePerHour;
+        public int NumOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CalculateWage(String Company, int empRatePerHour, int NumOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.Company = Company;
+            this.empRatePerHour = empRatePerHour;
+            this.NumOfWorkingDays = NumOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void ComputeEmpWage()
+        {
+            int empHours = 0, totalEmpHours = 0, totalWorkingDays = 0;
+            while (totalEmpHours <= this.maxHoursPerMonth && totalWorkingDays <= this.NumOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHours = 4;
+                        break;
+
+                    case isFullTime:
+                        empHours = 8;
+                        break;
+
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                totalEmpHours += empHours;
+            }
+            totalEmpWage = totalEmpHours * empRatePerHour;
+            //Console.WriteLine("total emp wage for company:" + Company + " is " + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "total emp wage for company:" + this.Company + " is " + this.totalEmpWage;
+        }
+    }
+}
+
+    
---
+++ EmployeeWage_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeWage_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateWage Google = new CalculateWage("google", 20, 20, 100);
+            CalculateWage infosys = new CalculateWage("infosys", 30, 25, 100);
+            Google.ComputeEmpWage();
+            Console.WriteLine(Google.toString());
+            infosys.ComputeEmpWage();
+            Console.WriteLine(infosys.toString());
+
+        }
+    }
+}
