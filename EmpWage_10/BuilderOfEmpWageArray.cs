e4b60f16533f0f90cff84c317c06d733867467fe
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-06 11:29:04 +0530

    EmpWageDoneBySavingWageForMultipleCmpnys

---
+++ EmpWage_10/BuilderOfEmpWageArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWage_10
+{
+    public class BuilderOfEmpWageArray
+    {
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+
+        private int NumOfCompanies = 0;
+        private CompanyEmpWage[] calculateCompanyWageArray;
+
+        public BuilderOfEmpWageArray()
+        {
+            this.calculateCompanyWageArray = new CompanyEmpWage[5];
+        }
+        public void addcompanyEmpWage(string company, int empRatePerHr, int NumOfWorkingDays, int MaxWorkingHrPerMonth)
+        {
+            calculateCompanyWageArray[this.NumOfCompanies] = new CompanyEmpWage(company, empRatePerHr, NumOfWorkingDays, MaxWorkingHrPerMonth);
+            NumOfCompanies++;
+        }
+        public void ComputeEmpWage()
+        {
+            for (int i = 0; i < NumOfCompanies; i++)
+            {
+                calculateCompanyWageArray[i].setTotalEmpWage(this.ComputeEmpWage(this.calculateCompanyWageArray[i]));
+                Console.WriteLine(this.calculateCompanyWageArray[i].toString());
+            }
+        }
+
+        public int ComputeEmpWage(CompanyEmpWage companyWage)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalEmpHrs <= companyWage.maxHoursPerMonth && totalWorkingDays < companyWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 4;
+                        break;
+                    case 2:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Employee Hours : " + empHrs);
+                //Console.ReadLine();                
+            }
+            return totalEmpHrs * companyWage.empRatePerHour;
+        }
+    }
+}
+
+
+
+
---
+++ EmpWage_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmpWage_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BuilderOfEmpWageArray builderOfEmpWageArray = new BuilderOfEmpWageArray();
+            builderOfEmpWageArray.addcompanyEmpWage("Google", 20, 100, 50);
+            builderOfEmpWageArray.addcompanyEmpWage("Reliance", 15, 50, 100);
+            builderOfEmpWageArray.addcompanyEmpWage("Infosys", 20, 100, 50);
+            builderOfEmpWageArray.ComputeEmpWage();
+        }
+    }
+}
